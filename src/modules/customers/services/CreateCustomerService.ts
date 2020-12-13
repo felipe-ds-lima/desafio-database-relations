209ea6fa4a520862c837be2f24914a3f2f7d1ca6
Felipe D S Lima <felipe@monts.com.br>
2020-12-12 22:30:23 -0300

    challange concluded

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerEmail = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkCustomerEmail) {
+      throw new AppError('E-mail is alread in use.');
+    }
+
+    const customer = await this.customersRepository.create({ email, name });
+
+    return customer;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,49 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const findProducts = await this.ormRepository.find({
+      where: { id: In(products.map(product => product.id)) },
+    });
+
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const updatedProducts = await Promise.all(
+      products.map(
+        async (productData): Promise<Product | undefined> => {
+          const product = await this.ormRepository.findOne(productData.id);
+
+          if (!product) {
+            return product;
+          }
+
+          product.quantity = productData.quantity;
+
+          await this.ormRepository.save(product);
+
+          return product;
+        },
+      ),
+    );
+
+    return updatedProducts as Product[];
   }
 }
 
